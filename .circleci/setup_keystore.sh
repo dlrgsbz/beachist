aa17f2270fd22a6eb95b3bd8e8efdddbec9b5624
Tjark Saul <mail@tjarksaul.de>
2021-05-15 12:30:04 +0200

    Setup keystore for release build

---
+++ .circleci/setup_keystore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$(pwd)"
+GRADLE_PROPERTIES=${DIR}"/gradle.properties"
+export GRADLE_PROPERTIES
+echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+if [[ ! -f "$GRADLE_PROPERTIES" ]]; then
+    echo "Gradle Properties does not exist"
+
+    echo "Creating Gradle Properties file..."
+    touch $GRADLE_PROPERTIES
+fi
+
+echo "Writing keys to gradle.properties..."
+echo "KEYSTORE_FILE=.signing/release.jks" >> ${GRADLE_PROPERTIES}
+echo "KEYSTORE_PASSWORD=$CLIENT_KEYSTORE_PASSWORD" >> ${GRADLE_PROPERTIES}
+echo "KEY_ALIAS=$KEY_ALIAS" >> ${GRADLE_PROPERTIES}
+echo "KEY_PASSWORD=$KEY_PASSWORD" >> ${GRADLE_PROPERTIES}
