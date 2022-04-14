9c2438b7744d88c12e96a133b5269da82d0e98f8
Tjark Saul <mail@appfel.dev>
2022-04-14 11:09:48 +0200

    Separate lambda functions to reduce size

--- lambdas/cdk/infra.ts
+++ lambdas/cdk/infra.ts
@@ -1,14 +1,16 @@
 import * as path from 'path'
-import { StackProps } from "./cdk"
-import { environmentProps, Stage, Timeouts } from './config'
-import { Stack } from 'aws-cdk-lib'
-import { Construct } from 'constructs'
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda'
-import { IotSql, TopicRule } from '@aws-cdk/aws-iot-alpha'
-import { IotRepublishMqttAction, LambdaFunctionAction, MqttQualityOfService } from '@aws-cdk/aws-iot-actions-alpha'
-import { Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda'
+
 import { Effect, PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam'
+import { IotRepublishMqttAction, LambdaFunctionAction, MqttQualityOfService } from '@aws-cdk/aws-iot-actions-alpha'
+import { IotSql, TopicRule } from '@aws-cdk/aws-iot-alpha'
+import { Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { Stage, Timeouts, environmentProps } from './config'
+
 import { CfnPolicy } from 'aws-cdk-lib/aws-iot'
+import { Construct } from 'constructs'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Stack } from 'aws-cdk-lib'
+import { StackProps } from './cdk'
 
 interface LambdaEnvs {
   STAGE: Stage
@@ -20,7 +22,7 @@ interface LambdaEnvs {
 }
 
 export class InfraStack extends Stack {
-  private readonly code: Code
+  private readonly props: StackProps
 
   private readonly lambdaEnvs: LambdaEnvs
 
@@ -28,13 +30,11 @@ export class InfraStack extends Stack {
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id)
-    const handlerPrefix = 'dist/index.'
-
-    this.code = Code.fromAsset(path.join(__dirname, '../dist/index.js.zip'))
+    this.props = props
 
     this.getThingShadowRoleArn = `'arn:aws:iam::${this.account}:role/get-thing-shadow'`
 
-    const iotPolicy = this.createIotPolicy(props)
+    const iotPolicy = this.createIotPolicy()
 
     this.lambdaEnvs = {
       STAGE: environmentProps.stage,
@@ -45,38 +45,39 @@ export class InfraStack extends Stack {
       IOT_POLICY_NAME: iotPolicy.policyName!,
     }
 
-    this.createGetFieldsHandler(handlerPrefix, props)
-    this.createCreateEntryHandler(handlerPrefix, props)
-    this.createCreateEventHandler(handlerPrefix, props)
-    this.createProvisioningHandler(handlerPrefix, props)
-    this.createInfoHandler(handlerPrefix, props)
-    this.createLastWillTopic(props)
+    this.createGetFieldsHandler()
+    this.createCreateEntryHandler()
+    this.createCreateEventHandler()
+    this.createProvisioningHandler()
+    this.createInfoHandler()
+    this.createLastWillTopic()
   }
 
-  private createLastWillTopic(props: StackProps) {
+  private createLastWillTopic() {
     new TopicRule(this, 'update-shadow-last-will-rule', {
-      topicRuleName: `beachist${props.stage}UpdateShadowLastWillRule`,
+      topicRuleName: `beachist${this.props.stage}UpdateShadowLastWillRule`,
       sql: IotSql.fromStringAsVer20160323(
-        `SELECT * FROM '+/connected/update'`
+        `SELECT * FROM '+/connected/update'`,
       ),
       actions: [new IotRepublishMqttAction(
         '$$aws/things/${topic(1)}/shadow/update',
-        { qualityOfService: MqttQualityOfService.AT_LEAST_ONCE }
-      )]
+        { qualityOfService: MqttQualityOfService.AT_LEAST_ONCE },
+      )],
     })
   }
 
-  private createGetFieldsHandler(handlerPrefix: string, props: StackProps) {
+  private createGetFieldsHandler() {
     const getFieldsHandler = this.lambdaFunction(
-      `${props.prefix}-get-fields`,
-      `${handlerPrefix}getFieldsHandler`,
+      `${this.props.prefix}-get-fields`,
+      path.join('iot-triggers', 'get-fields'),
+      `getFieldsHandler`,
       this.lambdaEnvs,
     )
 
     addIotPublishToTopicRole(getFieldsHandler)
 
     new TopicRule(this, 'get-fields-rule', {
-      topicRuleName: `beachist${props.stage}GetFieldsRule`,
+      topicRuleName: `beachist${this.props.stage}GetFieldsRule`,
       sql: IotSql.fromStringAsVer20160323(
         `SELECT
             topic(1) as iotThingName,
@@ -87,17 +88,18 @@ export class InfraStack extends Stack {
     })
   }
 
-  private createCreateEventHandler(handlerPrefix: string, props: StackProps) {
+  private createCreateEventHandler() {
     const createEventHandler = this.lambdaFunction(
-      `${props.prefix}-create-event`,
-      `${handlerPrefix}createEventHandler`,
+      `${this.props.prefix}-create-event`,
+      path.join('iot-triggers', 'create-event'),
+      `createEventHandler`,
       this.lambdaEnvs,
     )
 
     addIotPublishToTopicRole(createEventHandler)
 
     new TopicRule(this, 'create-event-rule', {
-      topicRuleName: `beachist${props.stage}CreateEventRule`,
+      topicRuleName: `beachist${this.props.stage}CreateEventRule`,
       sql: IotSql.fromStringAsVer20160323(
         `SELECT 
             id, type, date, 
@@ -109,17 +111,18 @@ export class InfraStack extends Stack {
     })
   }
 
-  private createCreateEntryHandler(handlerPrefix: string, props: StackProps) {
+  private createCreateEntryHandler() {
     const createEntryHandler = this.lambdaFunction(
-      `${props.prefix}-create-entry`,
-      `${handlerPrefix}createEntryHandler`,
+      `${this.props.prefix}-create-entry`,
+      path.join('iot-triggers', 'create-entry'),
+      `createEntryHandler`,
       this.lambdaEnvs,
-    );
+    )
 
     addIotPublishToTopicRole(createEntryHandler)
 
     new TopicRule(this, 'create-entry-rule', {
-      topicRuleName: `beachist${props.stage}CreateEntryRule`,
+      topicRuleName: `beachist${this.props.stage}CreateEntryRule`,
       sql: IotSql.fromStringAsVer20160323(
         `SELECT 
                     state, 
@@ -133,18 +136,19 @@ export class InfraStack extends Stack {
                   FROM '+/field/+/entry'`,
       ),
       actions: [new LambdaFunctionAction(createEntryHandler)],
-    });
+    })
   }
 
-  private createInfoHandler(handlerPrefix: string, props: StackProps) {
+  private createInfoHandler() {
     const appInfoUpdatedHandler = this.lambdaFunction(
-      `${props.prefix}-app-info-updated`,
-      `${handlerPrefix}appInfoUpdatedHandler`,
+      `${this.props.prefix}-app-info-updated`,
+      path.join('iot-triggers', 'app-info-updated'),
+      `appInfoUpdatedHandler`,
       this.lambdaEnvs,
     )
 
-    return new TopicRule(this, `${props.prefix}-app-info-rule`, {
-      topicRuleName: `vgb${props.stage}AppInfoTopicRule`,
+    return new TopicRule(this, `${this.props.prefix}-app-info-rule`, {
+      topicRuleName: `vgb${this.props.stage}AppInfoTopicRule`,
       sql: IotSql.fromStringAsVer20160323(
         `SELECT current.state.reported.appVersionCode AS appVersionCode,
                 current.state.reported.appVersion     AS appVersion,
@@ -157,41 +161,42 @@ export class InfraStack extends Stack {
          OR previous.state.reported.connected <> current.state.reported.connected`,
       ),
       actions: [new LambdaFunctionAction(appInfoUpdatedHandler)],
-    });
+    })
   }
 
-  private createProvisioningHandler(handlerPrefix: string, props: StackProps) {
+  private createProvisioningHandler() {
     const f = this.lambdaFunction(
-      `${props.prefix}-provisioning`,
-      `${handlerPrefix}provisioningHandler`,
-      this.lambdaEnvs
+      `${this.props.prefix}-provisioning`,
+      path.join('http', 'provisioning'),
+      `provisioningHandler`,
+      this.lambdaEnvs,
     )
 
     addIotShadowUpdateRole(f)
 
     f.addToRolePolicy(new PolicyStatement({
       actions: [
-        "iot:AttachPolicy",
-        "iot:AttachThingPrincipal",
-        "iot:AttachPrincipalPolicy",
-        "iot:AddThingToThingGroup",
-        "iot:CreateKeysAndCertificate",
-        "iot:CreatePolicy",
-        "iot:CreateThing",
-        "iot:CreateThingGroup",
-        "iot:CreateThingType",
-        "iot:DeleteCertificate",
-        "iot:DescribeEndpoint",
-        "iot:DescribeThing",
-        "iot:DescribeThingGroup",
-        "iot:DetachPolicy",
-        "iot:DetachThingPrincipal",
-        "iot:ListAttachedPolicies",
-        "iot:ListThings",
-        "iot:ListThingGroups",
-        "iot:ListThingsInThingGroup",
-        "iot:ListThingPrincipals",
-        "iot:UpdateCertificate",
+        'iot:AttachPolicy',
+        'iot:AttachThingPrincipal',
+        'iot:AttachPrincipalPolicy',
+        'iot:AddThingToThingGroup',
+        'iot:CreateKeysAndCertificate',
+        'iot:CreatePolicy',
+        'iot:CreateThing',
+        'iot:CreateThingGroup',
+        'iot:CreateThingType',
+        'iot:DeleteCertificate',
+        'iot:DescribeEndpoint',
+        'iot:DescribeThing',
+        'iot:DescribeThingGroup',
+        'iot:DetachPolicy',
+        'iot:DetachThingPrincipal',
+        'iot:ListAttachedPolicies',
+        'iot:ListThings',
+        'iot:ListThingGroups',
+        'iot:ListThingsInThingGroup',
+        'iot:ListThingPrincipals',
+        'iot:UpdateCertificate',
       ],
       effect: Effect.ALLOW,
       resources: ['*'],
@@ -200,9 +205,9 @@ export class InfraStack extends Stack {
     // todo: add function URL
   }
 
-  private createIotPolicy(props: StackProps): CfnPolicy {
-    return new CfnPolicy(this, `${props.prefix}-iot-policy`, {
-      policyName: `${props.prefix}-iot-policy`,
+  private createIotPolicy(): CfnPolicy {
+    return new CfnPolicy(this, `${this.props.prefix}-iot-policy`, {
+      policyName: `${this.props.prefix}-iot-policy`,
       policyDocument: new PolicyDocument({
         statements: [
           // todo: this should not be here
@@ -214,12 +219,12 @@ export class InfraStack extends Stack {
           new PolicyStatement({
             effect: Effect.ALLOW,
             actions: ['iot:GetThingShadow'],
-            resources: [`arn:aws:iot:${Stack.of(this).region}:${Stack.of(this).account}:thing/\${iot:ClientId}`]
+            resources: [`arn:aws:iot:${Stack.of(this).region}:${Stack.of(this).account}:thing/\${iot:ClientId}`],
           }),
           new PolicyStatement({
             effect: Effect.ALLOW,
             actions: ['iot:UpdateThingShadow'],
-            resources: [`arn:aws:iot:${Stack.of(this).region}:${Stack.of(this).account}:thing/\${iot:ClientId}`]
+            resources: [`arn:aws:iot:${Stack.of(this).region}:${Stack.of(this).account}:thing/\${iot:ClientId}`],
           }),
           new PolicyStatement({
             effect: Effect.ALLOW,
@@ -237,20 +242,24 @@ export class InfraStack extends Stack {
               `arn:aws:iot:${Stack.of(this).region}:${Stack.of(this).account}:topicfilter/\${iot:ClientId}/*`,
             ],
           }),
-        ]
-      })
+        ],
+      }),
     })
   }
 
-  lambdaFunction(name: string, handler: string, environment: LambdaEnvs): LambdaFunction {
-    return new LambdaFunction(this, name, {
+  private lambdaFunction(name: string, dir: string, handler: string, environment: LambdaEnvs): LambdaFunction {
+    return new NodejsFunction(this, name, {
       functionName: name,
+      entry: path.join(__dirname, '../src/', dir, 'index.ts'),
       runtime: Runtime.NODEJS_14_X,
-      code: this.code,
       handler,
       memorySize: 1024,
       timeout: Timeouts.lambdaTimeout,
       environment: environment as unknown as Record<string, string>,
+      bundling: {
+        minify: true,
+        sourceMap: this.props.stage === Stage.DEV,
+      },
     })
   }
 }
@@ -262,7 +271,7 @@ const addIotShadowUpdateRole = (f: LambdaFunction) => {
       resources: ['*'],
       effect: Effect.ALLOW,
     }),
-  );
+  )
 }
 
 const addIotPublishToTopicRole = (f: LambdaFunction) => {
@@ -272,5 +281,5 @@ const addIotPublishToTopicRole = (f: LambdaFunction) => {
       resources: ['*'],
       effect: Effect.ALLOW,
     }),
-  );
+  )
 }
--- lambdas/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { createEntryHandler } from './iot-triggers/create-entry'
-export { createEventHandler } from './iot-triggers/create-event'
-export { getFieldsHandler } from './iot-triggers/get-fields'
-export { provisioningHandler } from './http/provisioning'
-export { appInfoUpdatedHandler } from './iot-triggers/app-info-updated'
