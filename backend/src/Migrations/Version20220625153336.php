ac984701b2dfeea6102e97bcf41307b85971bdee
Tjark Saul <mail@appfel.dev>
2022-06-25 23:25:15 +0200

    Fix migration

--- backend/src/Migrations/Version20220625153336.php
+++ backend/src/Migrations/Version20220625153336.php
@@ -22,7 +22,7 @@ final class Version20220625153336 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE crew_info (date DATE NOT NULL, station_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', crew VARCHAR(255) NOT NULL, INDEX IDX_F2B5D74821BDB235 (station_id), PRIMARY KEY(station_id, date)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE crew_info (date CHAR(10) NOT NULL, station_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', crew VARCHAR(255) NOT NULL, INDEX IDX_F2B5D74821BDB235 (station_id), PRIMARY KEY(station_id, date)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE crew_info ADD CONSTRAINT FK_F2B5D74821BDB235 FOREIGN KEY (station_id) REFERENCES station (id)');
     }
 
