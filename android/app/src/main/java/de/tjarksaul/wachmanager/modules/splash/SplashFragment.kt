2ae8717788fee0d2ae0cd451a9dc6e78c91206c2
Tjark Saul <mail@appfel.dev>
2022-07-05 17:53:50 +0200

    Fix a crash in hideKeyboard

--- android/app/src/main/java/de/tjarksaul/wachmanager/modules/splash/SplashFragment.kt
+++ android/app/src/main/java/de/tjarksaul/wachmanager/modules/splash/SplashFragment.kt
@@ -54,10 +54,10 @@ class SplashFragment : BaseFragment() {
     }
 
     private fun hideKeyboard() {
-        val imm =
-            requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        val activity = this.activity ?: return
+        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         //Find the currently focused view, so we can grab the correct window token from it.
-        var view = requireActivity().currentFocus
+        var view = activity.currentFocus
         //If no view currently has focus, create a new one, just so we can grab a window token from it
         if (view == null) {
             view = View(activity)
